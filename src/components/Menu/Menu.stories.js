7d0da9697eb93b493f75fd90195bdf7a6d489293
Aiman Samad <aiman.samad@zemoga.com>
2021-04-01 10:20:44 -0500

    minor: WIP Storybook

---
+++ src/components/Menu/Menu.stories.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import Menu from './index';
+
+export const MenuStory = () => <Menu/>;
+
+// const Template = (args) => <Button {...args} />;
+
+// export const Primary = Template.bind({});
+// Primary.args = {
+//   primary: true,
+//   label: 'Button',
+// };
+
+// export const Secondary = Template.bind({});
+// Secondary.args = {
+//   label: 'Button',
+// };
+
+// export const Large = Template.bind({});
+// Large.args = {
+//   size: 'large',
+//   label: 'Button',
+// };
+
+// export const Small = Template.bind({});
+// Small.args = {
+//   size: 'small',
+//   label: 'Button',
+// };
